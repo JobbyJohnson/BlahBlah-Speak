cd9a46037046e84f69c34f646a8d4d8cf64cacce
thejobbyjohnson <jobbyjohnson1998@gmail.com>
2021-05-27 21:04:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv=document.querySelector("#output");
+var outputDiv=document.querySelector("#outputbox");
 
 // var serverURL = "https://api.funtranslations.com/translate/minion.json"
 var serverURL ="https://api.funtranslations.com/translate/ferb-latin.json"
